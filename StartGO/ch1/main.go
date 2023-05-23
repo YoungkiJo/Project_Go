c06f2d5ef01f922c43375433ca468f5ea6fa44a0
YoungkiJo <83441265+YoungkiJo@users.noreply.github.com>
2023-05-23 09:31:17 +0900

    ch1 3, 4강

--- StartGO/ch1/main.go
+++ StartGO/ch1/main.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"githubcode/StartGO/ch1/something"
+	"strings"
 )
 
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
 func main() {
+	// 함수 call 시 대문자 사용(public 처럼 사용하기)
 	fmt.Println("Hello World")
 
 	something.SayHello()
 	something.SayBye()
+
+	// 상수 변수 차이 표시
+	const name1 string = "nico" // 상수
+	var name2 string = "nico"   // 변수
+	name3 := "nico"             // 변수의 다른 표현
+	fmt.Println(name1)
+	fmt.Println(name2)
+	fmt.Println(name3)
+
+	fmt.Println(multiply(2, 2))
+
+	totalLength, upperName := lenAndUpper("nico")
+	fmt.Println(totalLength, upperName)
 
 }
