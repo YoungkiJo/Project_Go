907dd7a3993f3c4e4d8b9cf1784cfc1f46cdde6f
YoungkiJo <83441265+YoungkiJo@users.noreply.github.com>
2023-06-02 11:04:20 +0900

    ch2 마무리

--- StartGO/ch2/main.go
+++ StartGO/ch2/main.go
@@ -2,18 +2,48 @@ package main
 
 import (
 	"fmt"
-	accounts "githubcode/StartGO/ch2/accounts"
+	"githubcode/StartGO/ch2/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-	err := account.Withdraw(20)
+	// account := accounts.NewAccount("nico")
+	// account.Deposit(10)
+	// err := account.Withdraw(20)
+	// if err != nil {
+	// 	// log.Fatalln(err)
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(account.Balance(), account.Owner())
+	// account.ChangeOwner("John")
+	// fmt.Println(account.Balance(), account.Owner())
+
+	// dictionary := mydict.Dictionary{"first": "First word"}
+	// definition, err := dictionary.Search("first")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
+
+	// dictionary := mydict.Dictionary{}
+	// baseWord := "hello"
+	// dictionary.Add(baseWord, "First")
+	// err := dictionary.Update(baseWord, "Second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search(baseWord)
+	// fmt.Println(word)
+
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
-		// log.Fatalln(err)
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
-	fmt.Println(account.Balance(), account.Owner())
-	account.ChangeOwner("John")
-	fmt.Println(account.Balance(), account.Owner())
 }
