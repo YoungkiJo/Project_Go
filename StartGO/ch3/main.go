2bacd5bc9746a7307983029e41bf47bb193e6e16
YoungkiJo <83441265+YoungkiJo@users.noreply.github.com>
2023-06-05 10:34:45 +0900

    ch3 마무리

--- StartGO/ch3/main.go
+++ StartGO/ch3/main.go
@@ -6,11 +6,17 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	// var results = map[string]string{}
-	var results = make(map[string]string)
+	var results = map[string]string{}
+	// results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com",
 		"https://www.google.com",
@@ -24,23 +30,24 @@ func main() {
 	}
 
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
+// chanel을 send only로 구성하는 함수
+func hitURL(url string, c chan requestResult) {
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
